8867757a735f59593e44c6b3ca34372603a37ebc
Subhansh Naithani <subhanshnaithani@gmail.com>
2025-04-18 16:17:33 +0530

    update changes

--- Frontend/vite-project/src/App.jsx
+++ Frontend/vite-project/src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import  Start  from './pages/Start.jsx'
 import  UserLogin  from './pages/UserLogin.jsx'
 import  UserSignup  from './pages/UserSignup.jsx'
+import Home from './pages/Home.jsx'
 
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Start/>} />
+        <Route path="/" element={<Home/>} />
         <Route path="/userlogin" element={<UserLogin/>} />
         <Route path="/usersignup" element={<UserSignup/>} />  
       </Routes>
