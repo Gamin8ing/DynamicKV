7332e58869f5e8314cee695b84a6a1c6b29ea305
Subhansh Naithani <subhanshnaithani@gmail.com>
2025-04-18 17:44:00 +0530

    component folder included

--- Frontend/vite-project/src/App.jsx
+++ Frontend/vite-project/src/App.jsx
@@ -3,6 +3,8 @@ import { Route, Routes } from 'react-router-dom'
 import  UserLogin  from './pages/UserLogin.jsx'
 import  UserSignup  from './pages/UserSignup.jsx'
 import Home from './pages/Home.jsx'
+import AdminDashBoard from './pages/AdminDashBoard.jsx'
+import AdminRoute from './routes/AdminRoute.jsx'
 
 const App = () => {
   return (
@@ -11,6 +13,11 @@ const App = () => {
         <Route path="/" element={<Home/>} />
         <Route path="/userlogin" element={<UserLogin/>} />
         <Route path="/usersignup" element={<UserSignup/>} />  
+        <Route path="/admin/dashboard" element={
+             <AdminRoute>
+              <AdminDashBoard/>
+             </AdminRoute>
+          } />
       </Routes>
     </div>
   )
